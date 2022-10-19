992e379533cfd8ea48b235b897fb1d40c59b7b94
AminGanteng <89477551+AminGanteng@users.noreply.github.com>
2022-10-19 07:59:20 +0700

    Add files via upload

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,8 +1,8 @@
 import moment from 'moment-timezone'
 let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    let bcbg = 'https://telegra.ph/file/da19d1c4c1e23b8b9c72e.jpg'
-    conn.send3ButtonLoc(m.chat, bcbg, wm, `${text ? `${text}\n` : ''}┌─「 Tag All 」\n` + users.map(v => '│◦❒ @' + v.replace(/@.+/, '')).join`\n` + '\n└────','Menu', '.menu', 'Owner', '.owner', `\nKakek Gw Sugiono`, '.huuu', null,
+    let bcbg = 'https://telegra.ph/file/43333415b0be705896141.jpg'
+    conn.send3ButtonLoc(m.chat, bcbg, wm, `${text ? `${text}\n` : ''}┌─「 Tag All 」\n` + users.map(v => '│◦❒ @' + v.replace(/@.+/, '')).join`\n` + '\n└────','Menu', '.menu', 'Owner', '.owner', `\nAku Suka Asrori Amin`, '.huuu', null,
  /*let m.reply(`${text ? `${text}\n` : ''}┌─「 Tag All 」\n` + users.map(v => '│◦❒ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, */{
         mentions: users
     })
