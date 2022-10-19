82f6834c017cfdd5fdbdf538f9b98f04077027ed
AminGanteng <89477551+AminGanteng@users.noreply.github.com>
2022-10-19 10:37:40 +0700

    Add files via upload

--- plugins/info-rules.js
+++ plugins/info-rules.js
@@ -21,11 +21,8 @@ let rules = `➯ *Tɪᴅᴀᴋ Sᴘᴀᴍ Bᴏᴛ*
 ⟬Eᴍᴀɴɢ Nʏᴀ Lᴜ Sᴀᴘᴀ? Uᴅʜ Mɪɴᴛᴀ Mᴀᴋsᴀ Pᴜʟᴀ⟭
 ➯ *Hɪɴᴀ Bᴏᴛ Gᴡ*
 ⟬Nɢᴇʜɪɴᴀ Eʟɪᴛ,Bᴜᴋᴛɪ Sᴜʟɪᴛ⟭
-➯ *Bot Delay/Lama Bles?*
-⟬Lu Di Chet Ayang Lu Aja Lama Blesny⟭
 ➯ *Maksa Masukin Gc Lu*
 ⟬Minimal Donasi Lah Cuy Biar Cuan Gw⟭
-➯ *Chet Aneh² Di Cwe Gw, Gw Block Lu Hdup²*
 ⟬Mɪɴɪᴍᴀʟ Nɢᴏᴛᴀᴋ⟭
 ❏┳━━◩
 ┍┛
@@ -33,13 +30,13 @@ let rules = `➯ *Tɪᴅᴀᴋ Sᴘᴀᴍ Bᴏᴛ*
 └─┈⟅`
 let nth = `☰⟥⟝⟞⟝❨ *Rᴜʟᴇs Mʏ Bᴏᴛ* ❩⟞⟝⟞⟤☰`
 conn.send3ButtonImg(m.chat, `${pickRandom(flaaa2)}` + `${ucapan()} ` + `${name}`, nth, rules, 'Menu', '.menu', 'Owner', '.owner', 'Credit', '.credit', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: 'https://wa.me/p/5489299044451325/6281361281833',
+    mediaUrl: 'https://wa.me/p/5501589316623331/380994767998',
     mediaType: 2, 
     description: sgc,
     title: "Sᴇᴡᴀ Cᴇᴋ Dɪsɪɴɪ Cᴜʏ!!!",
     body: wm,
     thumbnail: fs.readFileSync('./media/qr.png'),
-    sourceUrl: 'https://wa.me/p/5489299044451325/6281361281833',
+    sourceUrl: 'https://wa.me/p/5501589316623331/380994767998',
      }}
   })
 }
